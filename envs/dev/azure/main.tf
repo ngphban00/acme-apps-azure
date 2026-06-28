1c174acae1ee81ccf9650562d829dd597e356451
An Nguyen <ngphban@gmail.com>
2026-06-28 16:41:10 +0000

    feat: initial azure dev environment consuming platform module

---
+++ envs/dev/azure/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  cloud {
+    organization = "ngphban"
+
+    workspaces {
+      name = "static-site-azure-dev"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "order_portal" {
+  source  = "app.terraform.io/ngphban/static-site/azurerm"
+  version = "~> 1.0"
+
+  name            = "acme-order-portal"
+  environment     = var.environment
+  cost_center     = var.cost_center
+  owner           = var.owner
+  azure_region    = var.azure_region
+  index_html_path = "${path.module}/index.html"
+}
